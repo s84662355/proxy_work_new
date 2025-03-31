fc53bc4830404f41a763a6e570552290545573bb
chenjiahao <271375247@qq.com>
2025-04-01 00:33:59 +0800

    整理代码

--- constant/Dynamic主账号相关常量.go
+++ constant/Dynamic主账号相关常量.go
@@ -5,12 +5,11 @@ import (
 )
 
 const (
-	VsIPTransitDynamicCacheRedisKeyPrefix     = "VsIPTransitDynamicPrefix_" // 主账号redis缓存的key前缀
-	FlowUserIdQueueSortedSet                  = "FlowUserIdQueueSortedSet"  ///主账号流量redis有序集合队列
-	VsIPTransitDynamicUpdateFlowRedisLock     = "updatedbflow_lock"         // 主账号更新流量的分布式锁前缀
-	VsIPTransitDynamicUpdateFlowRedisLockTtl  = 60 * time.Second            // 主账号更新流量的分布式锁的超时时间
-	VsIPTransitDynamicAutoBuyFlowRedisLock    = "BuyFlow__lock"             // 主账号自动购买流量的分布式锁前缀
-	VsIPTransitDynamicAutoBuyFlowRedisLockTtl = 60 * time.Second            ///主账号自动购买流量的分布式锁过期时间
+	VsIPTransitDynamicCacheRedisKeyPrefix  = "VsIPTransitDynamicPrefix_" // 主账号redis缓存的key前缀
+	FlowUserIdQueueSortedSet               = "FlowUserIdQueueSortedSet"  ///主账号流量redis有序集合队列
+	VsIPTransitDynamicUpdateFlowRedisLock  = "updatedbflow_lock"         // 主账号更新流量的分布式锁前缀
+	VsIPTransitDynamicAutoBuyFlowRedisLock = "BuyFlow__lock"             // 主账号自动购买流量的分布式锁前缀
+
 )
 
 const (
@@ -20,4 +19,6 @@ const (
 const (
 	VsIPTransitDynamicCacheRedisTtl           = 60 * 60 * time.Second ///主账号redis缓存的过期时间
 	VsIPTransitDynamicBatchUpdateDataLoopTime = 15 * 60 * time.Second
+	VsIPTransitDynamicUpdateFlowRedisLockTtl  = 60 * time.Second // 主账号更新流量的分布式锁的超时时间
+	VsIPTransitDynamicAutoBuyFlowRedisLockTtl = 60 * time.Second ///主账号自动购买流量的分布式锁过期时间
 )
